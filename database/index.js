658d5a380cf46064ea55273cdf78d0b85d6590c0
WolflanFreitas <wolflanf@gmail.com>
2022-12-16 20:00:46 -0300

    Suppress warning mongo

--- database/index.js
+++ database/index.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import config from "../config/mongo";
 
+mongoose.set("strictQuery", true);
+
 class Database {
     constructor() {
         this.connection = mongoose.connect(config.url);
